1751dbc27785d039f74a534407c25edcf106b6a2
harshitpal660 <palharshit239@gmail.com>
2023-12-23 00:39:03 +0530

    input box changes

--- src/Components/addTodo.js
+++ src/Components/addTodo.js
@@ -42,7 +42,7 @@ function AddTodo() {
       {/* Input field for entering todo */}
       <input
         type="text"
-        className="text-black bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+        className="text-white bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
         placeholder="Enter a Todo..."
         value={input}
         onKeyDown={handleKeyPress}
